0969b646ac605160136e57424bc2f694ab2eb020
Unknown <idrisadrien@hotmail.fr>
2017-10-20 17:46:48 +0200

    Add SplashScreen
    Don't work and I don't know why for the moment

--- app/src/main/java/com/example/idrisadrien/appusers/SplashScreen.java
+++ app/src/main/java/com/example/idrisadrien/appusers/SplashScreen.java
@@ -1,8 +1,30 @@
 package com.example.idrisadrien.appusers;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 /**
  * Created by idrisadrien on 20/10/2017.
  */
 
-public class SplashScreen {
+public class SplashScreen extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, ListUsersActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
 }
+
